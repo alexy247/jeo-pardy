7b3466bf7dd52f4836cdd703a28fb6c9aca6da0b
nikitapleshkanov <nikitapleshkanovttt@mail.ru>
2024-10-05 20:33:51 +0300

    first changes

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Title from './components/title/Title'
+
+function App() {
+  return (
+    <div className="background">
+        <Title/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/title/Title.tsx
@@ -0,0 +1,16 @@
+import './Title.css';
+
+function Title() {
+    return (
+        <div className="title">
+            <div className="title_first-line">
+                Своя
+            </div>
+            <div className="title_second-line">
+                игра
+            </div>
+        </div>
+    );
+}
+
+export default Title;
